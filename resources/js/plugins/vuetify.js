219190b6e43f6225cf4374634f41944daa715568
narita <88299864+narita-imot@users.noreply.github.com>
2022-01-24 16:50:27 +0900

    vuetifyの日本語設定

--- resources/js/plugins/vuetify.js
+++ resources/js/plugins/vuetify.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify/lib'
+import ja from 'vuetify/es5/locale/ja'
+
 
 Vue.use(Vuetify)
 
-const opts = {}
+const opts = {
+    lang: {
+        locales: {ja},
+        current: 'ja'
+    }
+}
 
 export default new Vuetify(opts)
